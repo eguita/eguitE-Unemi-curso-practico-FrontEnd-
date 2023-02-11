7007a6f907886a9dcd44463259b6b1bde4fd0afc
Egui <edgui-vm@hotmail.com>
2023-02-11 18:38:59 -0500

    cambios

--- src/entity/cuenta-banco.js
+++
@@ -1,9 +0,0 @@
-class CuentaBancaria {
-  constructor(titular, tipoCuenta, cuentaProducto) {
-    this.titular = titular
-    this.tipoCuenta = tipoCuenta
-    this.cuentaProducto = cuentaProducto
-  }
-}
-
-export default CuentaBancaria
